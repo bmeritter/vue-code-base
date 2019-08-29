bdae9de985ebaeb15228ad8ec26bad97547490f2
Qiqi Zhao <qiqzhao@thoughtworks.com>
2019-08-29 17:52:55 +0800

    use axios

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,35 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    {{info}}
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+  // @ is an alias to /src
+  import HelloWorld from "@/components/HelloWorld.vue";
+  import axios from "axios";
 
-export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
-};
+  export default {
+    name: "home",
+    components: {
+      HelloWorld
+    },
+    data() {
+      return {
+        info: {}
+      }
+    },
+    mounted() {
+      axios
+        .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+        .then(response => {
+          console.log('respons', response);
+          this.info = response.data.bpi;
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  };
 </script>
